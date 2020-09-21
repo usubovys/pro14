dd6a304a91b07d7abb5a4eb1a3795e5c70f28177
usubovy <yusif.usubov@gmail.com>
2020-09-21 14:49:57 +0200

    Refactoring: Updated the definition function user_stats

--- bikeshare.py
+++ bikeshare.py
@@ -203,7 +203,7 @@ def trip_duration_stats(df):
     print('-'*40)
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Calculates statistics on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
